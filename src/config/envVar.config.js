92daa825a538a90fbfd0425e5670c65b5b2f991d
Fab Dubin <fabien.dubin@gmail.com>
2025-05-29 00:10:02 +0200

    Initial commit

--- src/config/envVar.config.js
+++ src/config/envVar.config.js
@@ -5,7 +5,10 @@ export function getRecaptchaKey() {
 const FALLBACK_IMG = import.meta.env.VITE_FALLBACK_IMG;
 export { FALLBACK_IMG };
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://appsbymci-server-cfg5c4fcfvcveaen.germanywestcentral-01.azurewebsites.net/";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
 export { API_URL };
 
 const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS || "Pass123";
