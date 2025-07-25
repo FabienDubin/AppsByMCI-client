2b091ed2ed62292cd1e4579e186c915b183f3fba
Fab Dubin <fabien.dubin@gmail.com>
2025-07-25 11:11:23 +0200

    Patch Api access

--- src/config/envVar.config.js
+++ src/config/envVar.config.js
@@ -7,10 +7,10 @@ const FALLBACK_IMG =
   "https://storagemercedescla01.blob.core.windows.net/profiles/ChatGPT%20Image%20May%2028,%202025,%2011_35_00%20PM.png";
 export { FALLBACK_IMG };
 
-// const API_URL =
-//   import.meta.env.VITE_API_URL ||
-//   "https://appsbymci-server-cfg5c4fcfvcveaen.germanywestcentral-01.azurewebsites.net/";
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
+const API_URL =
+  import.meta.env.VITE_API_URL ||
+  "https://appsbymci-server-cfg5c4fcfvcveaen.germanywestcentral-01.azurewebsites.net/";
+// const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5005";
 export { API_URL };
 
 const DEFAULT_PASS = import.meta.env.VITE_DEFAULT_PASS || "Pass123";
